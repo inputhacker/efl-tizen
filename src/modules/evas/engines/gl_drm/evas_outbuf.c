a36813c5ba6b9e07f3a3090519b291a9abeaa687
Derek Foreman <derekf@osg.samsung.com>
2016-01-22 14:48:34 -0600

    gl_drm: stop trying to figure out buffer age if gl extension isn't present
    If we don't have the right extension for age we probably don't have
    partial updates either.  The code to get the age calculations right is
    actually much nastier than this anyway since gbm can hand back buffers
    in arbitrary order, and as many as it feels like.
    
    Change-Id: I22ecdfcf18e5bff5b955fc51936431b7bb4d882c

--- src/modules/evas/engines/gl_drm/evas_outbuf.c
+++ src/modules/evas/engines/gl_drm/evas_outbuf.c
@@ -688,25 +688,7 @@ evas_outbuf_buffer_state_get(Outbuf *ob)
    else if ((ob->swap_mode != MODE_AUTO) &&
             (ob->swap_mode != MODE_FULL))
      {
-        int delta;
-
-        delta = (ob->priv.last - ob->priv.curr +
-                 (ob->priv.last > ob->priv.last ?
-                     0 : ob->priv.num)) % ob->priv.num;
-
-        switch (delta)
-          {
-           case 0:
-             return MODE_COPY;
-           case 1:
-             return MODE_DOUBLE;
-           case 2:
-             return MODE_TRIPLE;
-           case 3:
-             return MODE_QUADRUPLE;
-           default:
-             return MODE_FULL;
-          }
+        return MODE_FULL;
      }
 
    return ob->swap_mode;
