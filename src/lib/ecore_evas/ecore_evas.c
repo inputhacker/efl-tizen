4056d668fedf60268848f61bc22265f8350e3a14
jiin.moon <jiin.moon@samsung.com>
2016-03-29 15:09:59 +0900

    [ecore_evas] Fix TC fail issue with evas_gl
    Change the priority of the wayland
    @tizen_fix
    
    Change-Id: Ie047e7c11b1371fa81d4e48f4238e8db2af12b73

--- src/lib/ecore_evas/ecore_evas.c
+++ src/lib/ecore_evas/ecore_evas.c
@@ -1058,6 +1058,11 @@ _ecore_evas_constructor_software_tbm(int x EINA_UNUSED, int y EINA_UNUSED, int w
 /* note: keep sorted by priority, highest first */
 static const struct ecore_evas_engine _engines[] = {
   /* unix */
+
+/*TIZEN_ONLY(20160329): Change the priority of the wayland */
+  {"wayland_egl", _ecore_evas_constructor_wayland_egl},
+  {"wayland_shm", _ecore_evas_constructor_wayland_shm},
+// ** //
   {"software_x11", _ecore_evas_constructor_software_x11},
   {"opengl_x11", _ecore_evas_constructor_opengl_x11},
   {"fb", _ecore_evas_constructor_fb},
@@ -1067,8 +1072,10 @@ static const struct ecore_evas_engine _engines[] = {
   {"opengl_glew", _ecore_evas_constructor_opengl_glew},
   {"opengl_cocoa", _ecore_evas_constructor_cocoa},
   {"psl1ght", _ecore_evas_constructor_psl1ght},
+/* TIZEN ONLY(20160329): Change the priority of the wayland
   {"wayland_shm", _ecore_evas_constructor_wayland_shm},
   {"wayland_egl", _ecore_evas_constructor_wayland_egl},
+*/
   {"drm", _ecore_evas_constructor_drm},
   {"gl_drm", _ecore_evas_constructor_opengl_drm},
   {"opengl_sdl", _ecore_evas_constructor_opengl_sdl},
