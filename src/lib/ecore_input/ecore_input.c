2cfa130e6eea9e72e0f1b1f9164a24732b4258d1
Shinwoo Kim <kimcinoo.efl@gmail.com>
2016-06-13 19:41:38 +0900

    Ecore_Input: define data type for joysticks
    Summary: This adds support for joysticks for ecore_input
    
    Reviewers: cedric, devilhorns, Sergeant_Whitespace, raster, thiepha, zmike, jpeg
    
    Reviewed By: thiepha, zmike, jpeg
    
    Subscribers: thiepha, stefan_schmidt, zmike, singh.amitesh, Sergeant_Whitespace, jgerecke, cedric, seoz
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D1538
    
    Change-Id: Iba40a1a4be0e0d830c3d09cb719179f4b27b9190

--- src/lib/ecore_input/ecore_input.c
+++ src/lib/ecore_input/ecore_input.c
@@ -26,6 +26,7 @@ EAPI int ECORE_EVENT_MOUSE_BUTTON_CANCEL = 0;
 EAPI int ECORE_EVENT_DEVICE_ADD = 0;
 EAPI int ECORE_EVENT_DEVICE_DEL = 0;
 EAPI int ECORE_EVENT_DETENT_ROTATE = 0; //TIZEN ONLY
+EAPI int ECORE_EVENT_JOYSTICK = 0;
 
 static int _ecore_event_init_count = 0;
 
@@ -61,6 +62,9 @@ ecore_event_init(void)
    ECORE_EVENT_DEVICE_ADD = ecore_event_type_new();
    ECORE_EVENT_DEVICE_DEL = ecore_event_type_new();
    ECORE_EVENT_DETENT_ROTATE = ecore_event_type_new(); //TIZEN ONLY
+   ECORE_EVENT_JOYSTICK = ecore_event_type_new();
+
+   ecore_input_joystick_init();
 
    return _ecore_event_init_count;
 }
@@ -84,6 +88,8 @@ ecore_event_shutdown(void)
    ECORE_EVENT_DEVICE_ADD = 0;
    ECORE_EVENT_DEVICE_DEL = 0;
    ECORE_EVENT_DETENT_ROTATE = 0; //TIZEN ONLY
+   ECORE_EVENT_JOYSTICK = 0;
+   ecore_input_joystick_shutdown();
    eina_log_domain_unregister(_ecore_input_log_dom);
    _ecore_input_log_dom = -1;
    ecore_shutdown();
