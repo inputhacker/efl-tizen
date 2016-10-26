4b06fdd7e6970620ac26b276832f7bcd4d033813
Joogab Yun <joogab.yun@samsung.com>
2016-10-26 13:10:47 +0900

    [evas_gl] change a max_memcpy_size 512 to 256
    Change-Id: I9a1c87c8a524c51fc39039397d91c4ad5dd08d2d

--- src/modules/evas/engines/gl_common/evas_gl_common.h
+++ src/modules/evas/engines/gl_common/evas_gl_common.h
@@ -155,7 +155,7 @@ struct _Evas_GL_Shared
 #define DEF_ATLAS_H                 512
 
 #define MAX_ATLAS_MEMCPY       1024
-#define DEF_ATLAS_MEMCPY       512
+#define DEF_ATLAS_MEMCPY       256
 
 #define ATLAS_FORMATS_COUNT    12
 
