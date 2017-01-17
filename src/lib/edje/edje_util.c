6204e73232395d4333095c944ff69fbcee1d6c1a
Jee-Yong Um <jc9.um@samsung.com>
2017-01-17 10:57:04 +0900

    edje: set repeat_events of swallowed object only when the part has value
    Signed-Off-By: Jee-Yong Um <jc9.um@samsung.com>
    
    Change-Id: I2b323825ec4d70a7d6209677eae4bb001a6b69d3

--- src/lib/edje/edje_util.c
+++ src/lib/edje/edje_util.c
@@ -6529,7 +6529,8 @@ _edje_real_part_swallow(Edje *ed,
    if (rp->mouse_events)
      {
         _edje_callbacks_add(obj_swallow, ed, rp);
-        evas_object_repeat_events_set(obj_swallow, rp->repeat_events);
+        if (rp->repeat_events)
+          evas_object_repeat_events_set(obj_swallow, rp->repeat_events);
         if (rp->part->pointer_mode != EVAS_OBJECT_POINTER_MODE_AUTOGRAB)
           evas_object_pointer_mode_set(obj_swallow, rp->part->pointer_mode);
         evas_object_pass_events_set(obj_swallow, 0);
