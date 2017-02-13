c7e4e83acc6389cb46b7fd88a50b81e56fd5f64b
Duna Oh <duna.oh@samsung.com>
2017-02-13 20:04:27 +0900

    ecore_wl: add codes to request set_geometry_hint when setting geometry hints according to the angle
    Signed-off-by: Duna Oh <duna.oh@samsung.com>
    
    Change-Id: Icba756ac6e0ebec734c686032662f8b3c6958e2a
    (cherry picked from commit 2193d19a09e73ebc484a67854ba9963f0d9fb7ca)

--- src/lib/ecore_wayland/tizen-policy-ext-protocol.c
+++ src/lib/ecore_wayland/tizen-policy-ext-protocol.c
@@ -10,14 +10,15 @@ static const struct wl_interface *types[] = {
 	NULL,
 	NULL,
 	NULL,
+	NULL,
 	&tizen_rotation_interface,
 	&wl_surface_interface,
 	&wl_surface_interface,
 };
 
 static const struct wl_message tizen_policy_ext_requests[] = {
-	{ "get_rotation", "no", types + 4 },
-	{ "get_active_angle", "?o", types + 6 },
+	{ "get_rotation", "no", types + 5 },
+	{ "get_active_angle", "?o", types + 7 },
 };
 
 static const struct wl_message tizen_policy_ext_events[] = {
@@ -25,7 +26,7 @@ static const struct wl_message tizen_policy_ext_events[] = {
 };
 
 WL_EXPORT const struct wl_interface tizen_policy_ext_interface = {
-	"tizen_policy_ext", 2,
+	"tizen_policy_ext", 3,
 	2, tizen_policy_ext_requests,
 	1, tizen_policy_ext_events,
 };
@@ -35,6 +36,7 @@ static const struct wl_message tizen_rotation_requests[] = {
 	{ "set_available_angles", "u", types + 0 },
 	{ "set_preferred_angle", "u", types + 0 },
 	{ "ack_angle_change", "u", types + 0 },
+	{ "set_geometry_hint", "3uuuuu", types + 0 },
 };
 
 static const struct wl_message tizen_rotation_events[] = {
@@ -45,8 +47,8 @@ static const struct wl_message tizen_rotation_events[] = {
 };
 
 WL_EXPORT const struct wl_interface tizen_rotation_interface = {
-	"tizen_rotation", 2,
-	4, tizen_rotation_requests,
+	"tizen_rotation", 3,
+	5, tizen_rotation_requests,
 	4, tizen_rotation_events,
 };
 
