3029728261b3192e7c8050c84c42efc7691c699c
Joogab Yun <joogab.yun@samsung.com>
2017-03-10 15:43:47 +0900

    [evas_engine] SW backend : fixed tbm leak
    if native surface set NULL, func.free is not called
    
    Change-Id: I687e494d68ccbf06e8577fa752f25f9a3139e0fa

--- src/modules/evas/engines/wayland_shm/evas_engine.c
+++ src/modules/evas/engines/wayland_shm/evas_engine.c
@@ -296,7 +296,7 @@ eng_image_native_set(void *data EINA_UNUSED, void *image, void *native)
    RGBA_Image *im = image, *im2 = NULL;
    void *wl_buf = NULL;
 
-   if (!im || !ns) return im;
+   if (!im) return im;
 
    if (ns)
      {
@@ -381,6 +381,8 @@ eng_image_native_set(void *data EINA_UNUSED, void *image, void *native)
          if (im->native.func.free)
             im->native.func.free(im->native.func.data, im);
       }
+
+   if (!ns) return im;
 
 #ifdef EVAS_CSERVE2
    if (evas_cserve2_use_get() && evas_cache2_image_cached(ie))
