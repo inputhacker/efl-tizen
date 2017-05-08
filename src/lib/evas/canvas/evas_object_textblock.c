d689a6193ec28596aff272d2fb196a3b023da105
Youngbok Shin <youngb.shin@samsung.com>
2017-05-08 10:00:38 +0900

    evas textblock: fix dereferenced after free issue for ellipsis item
    Change-Id: I31d6fa6afb8cfba015ff2e024cc933b9ecb02081

--- src/lib/evas/canvas/evas_object_textblock.c
+++ src/lib/evas/canvas/evas_object_textblock.c
@@ -5126,7 +5126,7 @@ _layout_par_ellipsis_items(Ctxt *c, double ellip)
         off += it->adv;
      }
    c->o->ellip_prev_it = i;
-   if (it) _layout_ellipsis_item_new(c, it);
+   if (it) ellip_ti = _layout_ellipsis_item_new(c, it);
 
 
    pos = (it && it->type == EVAS_TEXTBLOCK_ITEM_TEXT) ?
