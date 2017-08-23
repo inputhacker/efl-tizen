0e55af50a5b6a1136813425b636bb9ee78837a7c
Boram Park <boram1288.park@samsung.com>
2017-08-23 15:59:19 +0900

    no calling render function when output transformed
    If manual_render is set, client shouldn't render objects without app's
    request.
    
    Change-Id: I166ae9051a004883075fe0aabfaa930d6a2b3a0d

--- src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
+++ src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
@@ -517,7 +517,9 @@ _ecore_evas_wl_common_cb_output_transform(void *data, int type EINA_UNUSED, void
 
    _ecore_evas_wl_common_rotate_update(ee);
    evas_damage_rectangle_add(ee->evas, 0, 0, ee->w, ee->h);
-   _ecore_evas_wl_common_render(ee);
+
+   if (!ee->manual_render)
+     _ecore_evas_wl_common_render(ee);
 
    return ECORE_CALLBACK_PASS_ON;
 }
@@ -531,7 +533,9 @@ _ecore_evas_wl_common_cb_ignore_output_transform(void *data, int type EINA_UNUSE
 
    _ecore_evas_wl_common_rotate_update(ee);
    evas_damage_rectangle_add(ee->evas, 0, 0, ee->w, ee->h);
-   _ecore_evas_wl_common_render(ee);
+
+   if (!ee->manual_render)
+     _ecore_evas_wl_common_render(ee);
 
    return ECORE_CALLBACK_PASS_ON;
 }
