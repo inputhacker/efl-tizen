f6469955eaccf863211ffa825b65c8ebaae2598d
Joogab Yun <joogab.yun@samsung.com>
2017-09-01 16:01:19 +0900

    [evas_gl] fixed image corruption issue
    repeating evas_object_image_orient_set() and evas_object_imgae_data_get() breaks the
    image.
    so I modify the image rotated part like sw backend.
    
    Change-Id: I6f25ca4be4da2293a2b2cc8e8168d2c1b94aad32

---
+++ src/modules/evas/engines/software_generic/evas_engine.h
@@ -0,0 +1,39 @@
+#ifndef _EVAS_ENGINE_H
+# define _EVAS_ENGINE_H
+
+#ifdef EAPI
+# undef EAPI
+#endif
+
+#ifdef _WIN32
+# ifdef EFL_EVAS_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_EVAS_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif /* ! _WIN32 */
+
+
+EAPI void _image_flip_horizontal(DATA32 *pixels_out, const DATA32 *pixels_in, int iw, int ih);
+EAPI void _image_flip_vertical(DATA32 *pixels_out, const DATA32 *pixels_in, int iw, int ih);
+EAPI void _image_rotate_180(DATA32 *pixels_out, const DATA32 *pixels_in, int iw, int ih);
+EAPI void _image_rotate_90(DATA32 *pixels_out, const DATA32 *pixels_in, int iw, int ih);
+EAPI void _image_rotate_270(DATA32 *pixels_out, const DATA32 *pixels_in, int iw, int ih);
+EAPI void _image_flip_transpose(DATA32 *pixels_out, const DATA32 *pixels_in, int iw, int ih);
+EAPI void _image_flip_transverse(DATA32 *pixels_out, const DATA32 *pixels_in, int iw, int ih);
+
+#endif //_EVAS_ENGINE_H
