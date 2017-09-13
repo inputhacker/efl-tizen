57d03ea3134b81b567d5efb39c85cdb7c743f7a9
Jiyoun Park <jy0703.park@samsung.com>
2017-09-13 15:49:24 +0900

    eina file - use recursive locks for cache and file to avoid deadlock
       since we have a sigbusd handler that flags an eina file with io errors
       it has to walk the file cache and every file... taking locks. if those
       locks were taking already in the current thread the sighandler was
       called in... we'd deadlock. since this basicallly never happens (when
       do we see i/o errors really? not much)... we never saw this as it'd
       also reauire this race condition to happen too. but it is a problem
       waiting to happen. this fixes that by moving to recrusive locks.
    
       @fix
    
       opensource_commit 15cdd9b71b6d5abef5adc563410b4aa9a3443800
       Author: Carsten Haitzler (Rasterman) <raster@rasterman.com>
       Date:   Wed Aug 30 11:31:02 2017 +0900
    
    Change-Id: I7d66be8334a4a92c2b5083120939dbfa92e3b3a6

--- src/lib/eina/eina_file_common.c
+++ src/lib/eina/eina_file_common.c
@@ -402,7 +402,7 @@ eina_file_virtualize(const char *virtual_name, const void *data, unsigned long l
    else
      sprintf((char*) file->filename, tmpname, ti);
 
-   eina_lock_new(&file->lock);
+   eina_lock_recursive_new(&file->lock);
    file->mtime = ti / 1000;
    file->length = length;
 #ifdef _STAT_VER_LINUX
@@ -1025,7 +1025,7 @@ eina_file_init(void)
         return EINA_FALSE;
      }
 
-   eina_lock_new(&_eina_file_lock_cache);
+   eina_lock_recursive_new(&_eina_file_lock_cache);
 
    return EINA_TRUE;
 }
