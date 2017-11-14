71431bf7e713fa2cd838d21df1d351b573fc6770
Sung-Jin Park <sj76.park@samsung.com>
2017-11-14 09:35:31 +0900

    ecore_evas: transform the input event in ecore_drm (enlightenment) side
    Change-Id: Ic06d068aacba4a2f545b1ac4ae6d2568a8db8fcc
    Signed-off-by: Sung-Jin Park <sj76.park@samsung.com>

--- src/lib/ecore_evas/ecore_evas.c
+++ src/lib/ecore_evas/ecore_evas.c
@@ -3713,6 +3713,26 @@ _ecore_evas_mouse_move_with_multi_info_process(Ecore_Evas *ee, int x, int y, uns
 {
    _ecore_evas_mouse_move_process_with_multi_info_internal(ee, NULL, x, y, timestamp, radius, radius_x, radius_y, pressure, angle, EINA_TRUE);
 }
+
+EAPI void
+_ecore_evas_mouse_move_with_multi_info_process2(Ecore_Evas *ee, int x, int y, unsigned int timestamp, double radius, double radius_x, double radius_y, double pressure, double angle)
+{
+   int fx, fy, fw, fh;
+
+   ee->mouse.x = x;
+   ee->mouse.y = y;
+
+   evas_output_framespace_get(ee->evas, &fx, &fy, &fw, &fh);
+   if (ee->prop.cursor.object)
+     {
+        evas_object_show(ee->prop.cursor.object);
+        evas_object_move(ee->prop.cursor.object,
+                         x - fx - ee->prop.cursor.hot.x,
+                         y - fy - ee->prop.cursor.hot.y);
+     }
+
+   evas_event_input_mouse_move_with_multi_info(ee->evas, x, y, timestamp, NULL, radius, radius_x, radius_y, pressure, angle);
+}
 //
 
 EAPI void
