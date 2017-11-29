68cafd440a90798b34482f6f4b3d86750528c164
Shilpa Singh <shilpa.singh@samsung.com>
2017-11-29 20:17:26 +0530

    elm: atspi: Implicit declaration of function elm_atspi_bridge_utils_say, issue fix.
    Change-Id: I9bc6e2582351c09722f439d236f5daccd4a34f27

--- src/lib/elementary/elm_atspi_bridge.h
+++ src/lib/elementary/elm_atspi_bridge.h
@@ -1,5 +1,3 @@
-#ifdef EFL_BETA_API_SUPPORT
-#ifdef EFL_EO_API_SUPPORT
 #include "elm_atspi_bridge.eo.h"
 //TIZEN_ONLY(20160527) - Add direct reading feature
 typedef void (*Elm_Atspi_Say_Signal_Cb)(void *data, const char *say_signal);
@@ -8,8 +6,6 @@ EAPI void elm_atspi_bridge_utils_say(const char* text,
                                      const Elm_Atspi_Say_Signal_Cb func,
                                      const void *data);
 //
-#endif
 #ifndef EFL_NOLEGACY_API_SUPPORT
 #include "elm_atspi_bridge.eo.legacy.h"
-#endif
 #endif
