1269f6e2d5ec393add48dd6d9e4ffc299c7c9429
Gwanglim Lee <gl77.lee@samsung.com>
2017-11-29 17:38:05 +0900

    wayland_egl: calls ecore_wl2_window_commit before calling eglSwapBuffers to trigger next evas rendering.
    Change-Id: Ia68ee93c8207c0b83704c43019bd6cf9cfbb1735

--- src/modules/evas/engines/wayland_egl/evas_wl_main.c
+++ src/modules/evas/engines/wayland_egl/evas_wl_main.c
@@ -695,8 +695,8 @@ eng_outbuf_flush(Outbuf *ob, Tilebuf_Rect *surface_damage, Tilebuf_Rect *buffer_
 
 // TIZEN_ONLY(20171127) : wayland_egl bug fixed
 //   ecore_wl2_window_buffer_attach(ob->wl2_win, NULL, 0, 0, EINA_TRUE);
-//   ecore_wl2_window_commit(ob->wl2_win, EINA_FALSE);
 //
+   ecore_wl2_window_commit(ob->wl2_win, EINA_FALSE);
 
    if ((glsym_eglSwapBuffersWithDamage) && (surface_damage) &&
        (ob->swap_mode != MODE_FULL))
