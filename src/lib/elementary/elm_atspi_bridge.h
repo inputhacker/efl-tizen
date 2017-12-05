3ab5c6ba5dd20d2743436f729a7ab36797e71f68
Shinwoo Kim <cinoo.kim@samsung.com>
2017-12-05 21:17:58 +0900

    elm: fix build error
    Build error:
    /usr/include/elementary-1/elm_atspi_bridge.eo.h:42:20:
     error: 'Efl_Event_Description' does not name a type
    
    The Efl_Event_Description is defined in efl_object.eo.h which is included only
    if EFL_BETA_API_SUPPORT is defined in Eo.h. So the elm_atspi_bridge.eo.h should
    be included only after EFL_BETA_API_SUPPORT is defined.
    
    Eo_Event_Description was defined in Eo.h. So there was no build error so far.
    
    Change-Id: Iec6e6259a6b5e0b192837022ced23cb2bb3bcdb9

--- src/lib/elementary/elm_atspi_bridge.h
+++ src/lib/elementary/elm_atspi_bridge.h
@@ -1,4 +1,11 @@
+#ifdef EFL_BETA_API_SUPPORT
+#ifdef EFL_EO_API_SUPPORT
 #include "elm_atspi_bridge.eo.h"
+#endif
+#ifndef EFL_NOLEGACY_API_SUPPORT
+#include "elm_atspi_bridge.eo.legacy.h"
+#endif
+#endif
 //TIZEN_ONLY(20160527) - Add direct reading feature
 typedef void (*Elm_Atspi_Say_Signal_Cb)(void *data, const char *say_signal);
 EAPI void elm_atspi_bridge_utils_say(const char* text,
@@ -13,6 +20,3 @@ EAPI void elm_atspi_bridge_utils_say(const char* text,
  */
 EAPI Eina_Bool elm_atspi_bridge_utils_is_screen_reader_enabled(void);
 //
-#ifndef EFL_NOLEGACY_API_SUPPORT
-#include "elm_atspi_bridge.eo.legacy.h"
-#endif
