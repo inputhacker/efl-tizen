5b37d5ec2d3cea6c9b9ba4738aededd84934e619
sunghyun kim <scholb.kim@samsung.com>
2017-12-07 13:46:00 +0900

    [evasgl] Delete checking checksum when copy old shader
    when application use runtime shader, it add runtime shader when application shutdown.
    for add runtime shader, they copy old shader file to temp file  and modify,rename this.
    but in this case, eet_open & checksum check is failed.
    because temp file is empty (it didn't write checksum)
    so delete checking checksum when copy old shader to temp file.
    
    and..
    
    if new shader is compiled, it will be saved after shutdown.
    because of this, shader is compiled 2~3 times in some case.
    so add some code for this problem.
    1. close & reopen shader before checking shader file.
    2. update shader file when application compile new shader
    
    Change-Id: I3eb966d9f67695cb576db6b955824574251083b6

--- src/modules/evas/engines/gl_common/evas_gl_shader.c
+++ src/modules/evas/engines/gl_common/evas_gl_shader.c
@@ -340,16 +340,10 @@ _evas_gl_common_shader_binary_save(Evas_GL_Shared *shared)
    /* copy old file */
    if (copy)
      {
-        ef = eet_open(tmp_file_path, EET_FILE_MODE_READ);
-        if (!ef) goto save;
-        if (!_evas_gl_common_shader_binary_checksum_check(shared, ef))
-          copy = EINA_FALSE;
-        eet_close(ef);
-        if (copy)
-          eina_file_copy(bin_file_path, tmp_file_path, EINA_FILE_COPY_DATA, NULL, NULL);
+        if(!eina_file_copy(bin_file_path, tmp_file_path, EINA_FILE_COPY_DATA, NULL, NULL))
+          ERR("fail to copy old shader");
      }
 
-save:
    ef = eet_open(tmp_file_path, copy ? EET_FILE_MODE_READ_WRITE : EET_FILE_MODE_WRITE);
    if (!ef) goto error;
 
@@ -996,6 +990,9 @@ evas_gl_common_shader_program_get(Evas_Engine_GL_Context *gc,
                }
           }
         p = evas_gl_common_shader_generate_and_compile(gc->shared, flags);
+
+        //save shader after compile
+        evas_gl_common_shaders_flush(gc->shared);
         if (!p) return NULL;
      }
 end:
