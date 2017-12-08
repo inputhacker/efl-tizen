41b35e2a93bf42a26d42ce3ea4ddbba17f132167
Boram Park <boram1288.park@samsung.com>
2017-12-08 17:23:16 +0900

    ecore_evas_wl: add damage only when rendering
    Change-Id: I464887cc43a0053b1d3c89f4a0fd6224cd517c35

--- src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
+++ src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
@@ -3023,10 +3023,12 @@ _ecore_evas_wl_common_cb_output_transform(void *data, int type EINA_UNUSED, void
    if (output != ev->output) return ECORE_CALLBACK_PASS_ON;
 
    changed = _ecore_evas_wl_common_rotate_update(ee);
-   evas_damage_rectangle_add(ee->evas, 0, 0, ee->w, ee->h);
 
    if (!ee->manual_render && changed)
-     _ecore_evas_wl_common_render(ee);
+     {
+        evas_damage_rectangle_add(ee->evas, 0, 0, ee->w, ee->h);
+        _ecore_evas_wl_common_render(ee);
+     }
 
    return ECORE_CALLBACK_PASS_ON;
 }
@@ -3040,10 +3042,12 @@ _ecore_evas_wl_common_cb_ignore_output_transform(void *data, int type EINA_UNUSE
    if (!ee) return ECORE_CALLBACK_PASS_ON;
 
    changed = _ecore_evas_wl_common_rotate_update(ee);
-   evas_damage_rectangle_add(ee->evas, 0, 0, ee->w, ee->h);
 
    if (!ee->manual_render && changed)
-     _ecore_evas_wl_common_render(ee);
+     {
+        evas_damage_rectangle_add(ee->evas, 0, 0, ee->w, ee->h);
+        _ecore_evas_wl_common_render(ee);
+     }
 
    return ECORE_CALLBACK_PASS_ON;
 }
