4a994fe4b4b2e08c19b7a80c705b3835f966bf58
JengHyun Kang <jhyuni.kang@samsung.com>
2017-12-21 16:43:35 +0900

    evas_main: fix not to add evas devices for seat/mouse/keyboard even for software_tbm
    Change-Id: I605d6c0fed106ef457c5fa0176506af37b9ad230

--- src/lib/evas/canvas/evas_main.c
+++ src/lib/evas/canvas/evas_main.c
@@ -1051,7 +1051,8 @@ evas_output_method_set(Evas *eo_e, int render_method)
                           eina_streq(em->definition->name, "wayland_egl") ||
                           eina_streq(em->definition->name, "drm") ||
                           eina_streq(em->definition->name, "gl_tbm") ||
-                          eina_streq(em->definition->name, "gl_drm")))
+                          eina_streq(em->definition->name, "gl_drm") ||
+                          eina_streq(em->definition->name, "software_tbm")))
      {
         Evas_Pointer_Seat *pseat = calloc(1, sizeof(Evas_Pointer_Seat));
         e->seats = eina_inlist_append(e->seats, EINA_INLIST_GET(pseat));
