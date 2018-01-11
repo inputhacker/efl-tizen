f8ec5f2538a45ba490432c5b3cde36cfc3d0fd46
JunsuChoi <jsuya.choi@samsung.com>
2018-01-11 14:36:48 +0900

    elc_popup: read 'Alert' here temporary
    Change-Id: I84aa35a605924d7a00684d64d8ebbf324ba68b5f

--- src/lib/elementary/elc_popup.c
+++ src/lib/elementary/elc_popup.c
@@ -407,7 +407,7 @@ _access_info_cb(void *data, Evas_Object *obj EINA_UNUSED)
    if (priv->title_text)
      {
         buf = eina_strbuf_new();
-        eina_strbuf_append_printf(buf, "%s, %s", priv->title_text, N_("Title"));
+        eina_strbuf_append_printf(buf, "%s, %s, %s", N_("Alert"), priv->title_text, N_("Title"));
         ret = eina_strbuf_string_steal(buf);
         eina_strbuf_free(buf);
         return strdup(ret);
