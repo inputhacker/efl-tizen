531df789896ca2c35764e38714d1f486aff8b6e6
Youngbok Shin <youngb.shin@samsung.com>
2018-01-16 16:30:34 +0900

    evas: fix cursor_line_coord_set fail when y is 0.
    Signed-off-by: Vladyslav Shevchenko <v.shevchenko@samsung.com>
    origin: spin (tizen_2.4 branch)

--- src/lib/evas/canvas/evas_object_textblock.c
+++ src/lib/evas/canvas/evas_object_textblock.c
@@ -13129,7 +13129,22 @@ evas_textblock_cursor_line_coord_set(Evas_Textblock_Cursor *cur, Evas_Coord y)
         _layout_paragraph_render(o, found_par);
         EINA_INLIST_FOREACH(found_par->lines, ln)
           {
+             /* TIZEN_ONLY(20150626): fix cursor_line_coord_set fail when y is 0.
              if (ln->par->y + ln->y > y) break;
+              */
+             if (ln->par->y + ln->y > y)
+               {
+                  if (ln->par->line_no + ln->line_no == 0)
+                    {
+                       evas_textblock_cursor_line_set(cur,
+                                                      ln->par->line_no + ln->line_no);
+
+                       return ln->par->line_no + ln->line_no;
+                     }
+                  break;
+               }
+             /* END */
+
              if ((ln->par->y + ln->y <= y) && ((ln->par->y + ln->y + ln->h) > y))
                {
                   evas_textblock_cursor_line_set(cur, ln->par->line_no +
