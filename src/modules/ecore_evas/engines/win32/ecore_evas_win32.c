52091b4c284056bd64bd68acbf40b06668647bbe
Vincent Torri <vincent.torri@gmail.com>
2018-02-07 22:10:24 +0900

    Evas: remove depth usage in GDI and DirectDraw engines
    Test Plan: compilation and elementary_test
    
    Reviewers: raster
    
    Reviewed By: raster
    
    Subscribers: cedric, raster
    
    Differential Revision: https://phab.enlightenment.org/D5792

--- src/modules/ecore_evas/engines/win32/ecore_evas_win32.c
+++ src/modules/ecore_evas/engines/win32/ecore_evas_win32.c
@@ -1336,7 +1336,6 @@ _ecore_evas_engine_software_gdi_init(Ecore_Evas *ee)
      {
         /* FIXME: REDRAW_DEBUG missing for now */
         einfo->info.window = ((Ecore_Win32_Window *)ee->prop.window)->window;
-        einfo->info.depth = ecore_win32_screen_depth_get();
         einfo->info.rotation = 0;
         einfo->info.borderless = 0;
         einfo->info.fullscreen = 0;
@@ -1379,7 +1378,6 @@ _ecore_evas_engine_software_ddraw_init(Ecore_Evas *ee)
      {
         /* FIXME: REDRAW_DEBUG missing for now */
         einfo->info.window = ((Ecore_Win32_Window *)ee->prop.window)->window;
-        einfo->info.depth = ecore_win32_screen_depth_get();
         einfo->info.rotation = 0;
         if (!evas_engine_info_set(ee->evas, (Evas_Engine_Info *)einfo))
           {
