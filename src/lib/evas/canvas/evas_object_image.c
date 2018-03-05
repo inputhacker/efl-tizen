99893cd7682f6c6b49ecc5f16bfa65e863c8e79c
Joogab Yun <joogab.yun@samsung.com>
2018-03-05 19:08:03 +0900

    evas : prevent null of get_pixels call back
    Change-Id: I7d524dd90dcc772b753a6f11edfb8aa39559d09f

--- src/lib/evas/canvas/evas_object_image.c
+++ src/lib/evas/canvas/evas_object_image.c
@@ -3126,10 +3126,14 @@ evas_process_dirty_pixels(Evas_Object *eo_obj, Evas_Object_Protected_Data *obj, 
                   if (ENFN->gl_get_pixels_pre)
                     ENFN->gl_get_pixels_pre(output);
                   // IS INDIRECT RENDERING #1
-                  if (ENFN->gl_get_pixels)
-                    ENFN->gl_get_pixels(output, o->pixels->func.get_pixels, o->pixels->func.get_pixels_data, eo_obj, o->engine_data);
-                  else
-                  o->pixels->func.get_pixels(o->pixels->func.get_pixels_data, eo_obj);
+                  if (o->pixels->func.get_pixels)
+                    {
+                      if (ENFN->gl_get_pixels)
+                        ENFN->gl_get_pixels(output, o->pixels->func.get_pixels, o->pixels->func.get_pixels_data, eo_obj, o->engine_data);
+                      else
+                        o->pixels->func.get_pixels(o->pixels->func.get_pixels_data, eo_obj);
+                    }
+
                   if (ENFN->gl_get_pixels_post)
                     ENFN->gl_get_pixels_post(output);
                }
