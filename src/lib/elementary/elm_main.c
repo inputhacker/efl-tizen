ce563a625ff7fc851b2f2f026dd1a3da59632774
Cedric Bail <cedric@osg.samsung.com>
2018-03-07 09:34:49 -0800

    elementary: fix backward compatibility bug introduced by 9c8749b99a03d3601321da6d16071dd7b631d1ae.

--- src/lib/elementary/elm_main.c
+++ src/lib/elementary/elm_main.c
@@ -393,6 +393,9 @@ _sys_lang_changed(void *data EINA_UNUSED, int type EINA_UNUSED, void *event EINA
    return ECORE_CALLBACK_PASS_ON;
 }
 
+// This is necessary to keep backward compatibility
+static const char *bcargv[] = { "exe" };
+
 EAPI int
 elm_init(int argc, char **argv)
 {
@@ -411,8 +414,8 @@ elm_init(int argc, char **argv)
    //
         return _elm_init_count;
      }
-   elm_quicklaunch_init(argc, argv);
-   elm_quicklaunch_sub_init(argc, argv);
+   elm_quicklaunch_init(argc, argv ? argv : (char**) bcargv);
+   elm_quicklaunch_sub_init(argc, argv ? argv : (char**) bcargv);
 
    _prefix_shutdown();
 
