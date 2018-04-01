295acee4481888e31e4ce70ef75f036e45e88d22
Marcel Hollerbach <marcel@osg.samsung.com>
2018-04-01 20:55:34 +0200

    elm_entry: use a normal calc manager instead of the root focus
    fix T6795

--- src/lib/elementary/elm_entry.c
+++ src/lib/elementary/elm_entry.c
@@ -7749,6 +7749,14 @@ _elm_entry_efl_access_widget_action_elm_actions_get(Eo *obj EINA_UNUSED, Elm_Ent
    return &atspi_actions[0];
 }
 
+EOLIAN static Efl_Ui_Focus_Manager*
+_elm_entry_efl_ui_widget_focus_manager_create(Eo *obj, Elm_Entry_Data *pd EINA_UNUSED, Efl_Ui_Focus_Object *root)
+{
+   return efl_add(EFL_UI_FOCUS_MANAGER_CALC_CLASS, obj,
+     efl_ui_focus_manager_root_set(efl_added, root)
+   );
+}
+
 /////////////////////////////////////////////////////////////////
 // TIZEN_ONLY(20170512): Support accessibility for entry anchors.
 /////////////////////////////////////////////////////////////////
@@ -9092,9 +9100,7 @@ _elm_entry_efl_ui_widget_show_region_get(Eo *obj EINA_UNUSED, Elm_Entry_Data *sd
  * END *
  *******/
 
-
 /* Efl.Part begin */
-
 ELM_PART_OVERRIDE(elm_entry, ELM_ENTRY, Elm_Entry_Data)
 ELM_PART_OVERRIDE_CONTENT_SET(elm_entry, ELM_ENTRY, Elm_Entry_Data)
 ELM_PART_OVERRIDE_CONTENT_UNSET(elm_entry, ELM_ENTRY, Elm_Entry_Data)
