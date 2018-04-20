107b477bfd0a6a7ef9406b065e112b64525db8df
Hermet Park <hermetpark@gmail.com>
2018-04-20 11:44:43 +0900

    edje: Don't access a garbage pointer.
    set null to avoid multiple access.
    At 221 line, it tries to free tmp again.
    
    Change-Id: Ia995bc5fb69ad5a7aab1adf93eeba6ad9bf2617c

--- src/bin/edje/edje_cc_script.c
+++ src/bin/edje/edje_cc_script.c
@@ -105,6 +105,7 @@ code_parse_internal(Code *code)
           {
              if (tmp->str) free(tmp->str);
              free(tmp);
+             tmp = NULL;
           }
 
         switch (token->type)
