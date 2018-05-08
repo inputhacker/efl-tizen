c5183bc2e956e278d27c9e56c02385a576b543aa
SangHyeon Lee <sh10233.lee@samsung.com>
2018-05-08 19:17:00 +0900

    genlist : fix ABI break in gen common
    Change-Id: I469974e5885844e9da7cad5c33cfcf540904bedf

--- src/lib/elementary/elm_gen.h
+++ src/lib/elementary/elm_gen.h
@@ -113,10 +113,10 @@ typedef struct _Elm_Gen_Item_Class
                 * ELM_GENLIST/GENGRID_ITEM_CLASS_VERSION. */
   unsigned int refcount; /**< Set it to 0 if you use your own const class, or
                           * its managed for you by class ref/unref calls. */
-  Eina_Bool delete_me; /**< Leave this alone - set it to 0 if you have a const
+  Eina_Bool delete_me : 1; /**< Leave this alone - set it to 0 if you have a const
                         * class of your own. */
 // TIZEN ONLY(20160630): Support homogeneous mode in item class.
-   Eina_Bool     homogeneous : 1;
+   Eina_Bool homogeneous : 1;
 //
   const char *item_style; /**< Name of the visual style to use for this item. If
                            * you don't know use "default". */
