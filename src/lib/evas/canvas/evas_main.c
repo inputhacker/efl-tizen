e166a9b339367cd8951f331893259430bb72e0f3
Marcel Hollerbach <marcel@osg.samsung.com>
2018-05-10 11:55:43 +0200

    evas: introduce a log domain to monitor which object gets focus

--- src/lib/evas/canvas/evas_main.c
+++ src/lib/evas/canvas/evas_main.c
@@ -93,6 +93,7 @@ evas_init(void)
 		   EINA_LOG_STATE_INIT);
 
    _efl_gfx_map_init();
+   evas_focus_init();
 
    return _evas_init_count;
 
@@ -134,6 +135,8 @@ evas_shutdown(void)
    eina_log_timing(_evas_log_dom_global,
                    EINA_LOG_STATE_START,
                    EINA_LOG_STATE_SHUTDOWN);
+
+   evas_focus_shutdown();
 
    evas_common_shutdown();
 
