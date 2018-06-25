d5ee1b2fba4063eb487e87aa97bb0d0e985ff6c2
Chris Michael <cp.michael@samsung.com>
2018-06-25 11:29:26 -0400

    elm_access: Wrap calls to edje_object_part_object_get with freeze/thaw
    Summary:
    @ref T6884
    
    Depends on D6407
    
    Reviewers: zmike, ManMower
    
    Reviewed By: zmike
    
    Subscribers: cedric, #committers
    
    Tags: #efl
    
    Maniphest Tasks: T6884
    
    Differential Revision: https://phab.enlightenment.org/D6408

--- src/lib/elementary/elm_access.c
+++ src/lib/elementary/elm_access.c
@@ -1018,7 +1018,9 @@ _elm_access_edje_object_part_object_register(Evas_Object* obj,
 {
    Evas_Object *ao, *po;
 
+   edje_object_freeze((Evas_Object *)eobj);
    po = (Evas_Object *)edje_object_part_object_get(eobj, part);
+   edje_object_thaw((Evas_Object *)eobj);
    if (!obj || !po) return NULL;
 
    /* check previous access object */
@@ -1039,7 +1041,9 @@ _elm_access_edje_object_part_object_unregister(Evas_Object* obj EINA_UNUSED,
 {
    Evas_Object *po;
 
+   edje_object_freeze((Evas_Object *)eobj);
    po = (Evas_Object *)edje_object_part_object_get(eobj, part);
+   edje_object_thaw((Evas_Object *)eobj);
    if (!po) return;
 
    _access_object_unregister(po);
