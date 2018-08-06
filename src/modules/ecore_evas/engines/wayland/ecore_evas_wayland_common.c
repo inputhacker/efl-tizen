8feceaaa8811d3698f2ae03ed00e2d421fd72928
Mike Blumenkrantz <zmike@samsung.com>
2018-08-06 15:18:14 -0400

    ecore-evas/wayland: fix parent setting on canvas creation
    Summary:
    use the correct pointer when applying the passed parent object in order to
    successfully set the parent
    
    ref 78f27a3efff24ba3b935cc9e2620718e51af9304
    
    Reviewers: devilhorns
    
    Reviewed By: devilhorns
    
    Subscribers: cedric, #committers
    
    Tags: #efl_display_system
    
    Differential Revision: https://phab.enlightenment.org/D6757
    
    Change-Id: Iad0aeeb7542a3e6145c986434aa92b5d8a97fd3f

--- src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
+++ src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
@@ -3308,7 +3308,7 @@ Ecore_Evas *
 _ecore_evas_wl_common_options_new_internal(const char *disp_name, unsigned int parent, int x, int y, int w, int h, Eina_Bool frame, const int *opt, const char *engine_name)
 {
    Ecore_Wl2_Display *ewd;
-   Ecore_Wl2_Window *p = NULL;
+   Ecore_Wl2_Window *p = (Ecore_Wl2_Window *)parent;
    Evas_Engine_Info_Wayland *einfo;
    Ecore_Evas_Engine_Wl_Data *wdata;
    Ecore_Evas_Interface_Wayland *iface;
@@ -3390,11 +3390,7 @@ _ecore_evas_wl_common_options_new_internal(const char *disp_name, unsigned int p
    else
      ee->can_async_render = 1;
 
-   if (parent)
-     {
-        p = ecore_wl2_display_window_find(ewd, parent);
-        ee->alpha = ecore_wl2_window_alpha_get(p);
-     }
+   if (p) ee->alpha = ecore_wl2_window_alpha_get(p);
 
    wdata->sync_done = EINA_FALSE;
    wdata->parent = p;
