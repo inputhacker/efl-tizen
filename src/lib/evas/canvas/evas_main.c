8876770589c7ad4b1a55bb7c463a3fe88f6d2e17
Yeongjong Lee <yj34.lee@samsung.com>
2018-08-09 11:19:46 -0400

    evas: add safety check code to avoid crash
    Test Plan: evas_smart_objects_calculate(NULL);
    
    Reviewers: zmike, Hermet, devilhorns
    
    Reviewed By: devilhorns
    
    Subscribers: cedric, #reviewers, #committers
    
    Tags: #efl
    
    Differential Revision: https://phab.enlightenment.org/D6805

--- src/lib/evas/canvas/evas_main.c
+++ src/lib/evas/canvas/evas_main.c
@@ -1914,6 +1914,7 @@ _evas_canvas_efl_canvas_scene_smart_objects_calculate(Eo *eo_e, Evas_Public_Data
 EAPI void
 evas_smart_objects_calculate(Eo *eo_e)
 {
+   EINA_SAFETY_ON_NULL_RETURN(eo_e);
    evas_call_smarts_calculate(eo_e);
 }
 
