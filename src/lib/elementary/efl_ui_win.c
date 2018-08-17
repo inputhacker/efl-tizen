854f9432bb2d50ce9bade7af65e87a0095f324d3
Shinwoo Kim <cinoo.kim@samsung.com>
2018-08-17 20:20:39 +0900

    efl_ui_win: check window data before using it
    If user calls elm_win_focus_get with non-window object,
    then window data would be NULL which makes a crash.
    
    Change-Id: I3978f3058a63d6199fc656c710bf30003b4d1c30

--- src/lib/elementary/efl_ui_win.c
+++ src/lib/elementary/efl_ui_win.c
@@ -9879,11 +9879,13 @@ elm_win_socket_listen(Efl_Ui_Win *obj, const char *svcname, int svcnum, Eina_Boo
 EAPI Eina_Bool
 elm_win_focus_get(const Efl_Ui_Win *obj)
 {
+   //TIZEN_ONLY(20180817) check window data
    if (elm_widget_is_legacy(obj))
      {
-        ELM_WIN_DATA_GET(obj, sd);
+        ELM_WIN_DATA_GET_OR_RETURN(obj, sd, EINA_FALSE);
         return ecore_evas_focus_get(sd->ee);
      }
+   //
    return efl_ui_focus_object_focus_get(obj);
 }
 
