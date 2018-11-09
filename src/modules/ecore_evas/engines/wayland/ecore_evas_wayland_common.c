8d6d0dcec59c467f92205184b4e8e1fadd0b46cb
joogab yun <joogab.yun@samsung.com>
2018-11-09 09:26:33 +0000

    Revert "bug fixed : ignore_output_transform is should not processed."
    This reverts commit 8df577e6384201532109cbd02c4a7f704f143243.
    
    Change-Id: Ica8397ef000c3b7b1cd07415746f4bd6f4cf8cb9

--- src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
+++ src/modules/ecore_evas/engines/wayland/ecore_evas_wayland_common.c
@@ -120,7 +120,7 @@ _ecore_evas_wl_common_rotate_update(Ecore_Evas *ee)
    wdata = ee->engine.data;
 
    if (ecore_wl_window_ignore_output_transform_get(wdata->win))
-     return EINA_FALSE;
+     rotation = 0;
    else
      {
         Ecore_Wl_Output *output = ecore_wl_window_output_find(wdata->win);
