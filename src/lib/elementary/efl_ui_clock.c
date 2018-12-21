2f9e1438f01524031d54fc095dcb58f25db8d489
Cedric BAIL <cedric.bail@free.fr>
2018-12-21 14:46:10 -0800

    elementary: rename Efl.Ui.Translatable -> Efl.Ui.L10n
    If we are to choose Efl.Ui.I18n for the internationalization support, the localization API
    should be named accordingly L10n.
    
    Reviewed-by: Xavi Artigas <xavierartigas@yahoo.es>
    Differential Revision: https://phab.enlightenment.org/D7507

--- src/lib/elementary/efl_ui_clock.c
+++ src/lib/elementary/efl_ui_clock.c
@@ -3,7 +3,7 @@
 #endif
 
 #define EFL_ACCESS_OBJECT_PROTECTED
-#define EFL_UI_TRANSLATABLE_PROTECTED
+#define EFL_UI_L10N_PROTECTED
 
 #include <Elementary.h>
 #include "elm_priv.h"
@@ -527,12 +527,12 @@ _reload_format(Evas_Object *obj)
 }
 
 EOLIAN static void
-_efl_ui_clock_efl_ui_translatable_translation_update(Eo *obj, Efl_Ui_Clock_Data *sd)
+_efl_ui_clock_efl_ui_l10n_translation_update(Eo *obj, Efl_Ui_Clock_Data *sd)
 {
    if (!sd->user_format) _reload_format(obj);
    else _field_list_display(obj);
 
-   efl_ui_translatable_translation_update(efl_super(obj, MY_CLASS));
+   efl_ui_l10n_translation_update(efl_super(obj, MY_CLASS));
 }
 
 static Eina_List *
