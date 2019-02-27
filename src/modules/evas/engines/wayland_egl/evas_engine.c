bd0c6af6644f0d21d603e904baecee2f7d1f5444
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2019-02-27 15:49:19 +0000

    evas gl - swap with damage - look for KHR extn string too
    as per shortlog - look for KHR in addition to EXT for swap with damage
    region extn detection.

--- src/modules/evas/engines/wayland_egl/evas_engine.c
+++ src/modules/evas/engines/wayland_egl/evas_engine.c
@@ -240,7 +240,8 @@ gl_extn_veto(Render_Engine *re)
                 )
                extn_have_y_inverted = EINA_FALSE;
           }
-        if (!strstr(str, "EGL_EXT_swap_buffers_with_damage"))
+        if ((!strstr(str, "EGL_EXT_swap_buffers_with_damage")) &&
+            (!strstr(str, "EGL_KHR_swap_buffers_with_damage")))
           {
              glsym_eglSwapBuffersWithDamage = NULL;
           }
