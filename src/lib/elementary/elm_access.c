0bfd0f53ba7217fd53952b78392d3318d57f5668
Mike Blumenkrantz <zmike@samsung.com>
2019-03-05 17:00:37 -0500

    elm_access: remove all legacy usage from eo files
    this takes the current generated output from eolian for legacy code in
    efl and adds it to the tree, then removes legacy references from the
    corresponding eo files. in the case where the entire eo file was for
    a legacy object, that eo file has been removed from the tree
    
    ref T7724
    
    Reviewed-by: Cedric BAIL <cedric.bail@free.fr>
    Differential Revision: https://phab.enlightenment.org/D8152
    
    Change-Id: I33688c4f6c4357fe708968f413a7796c83b53c8f

--- src/lib/elementary/elm_access.c
+++ src/lib/elementary/elm_access.c
@@ -1618,4 +1618,4 @@ _elm_access_efl_access_object_description_get(const Eo *obj, void *pd EINA_UNUSE
 #define ELM_ACCESS_EXTRA_OPS \
    EFL_CANVAS_GROUP_ADD_OPS(elm_access)
 
-#include "elm_access.eo.c"
+#include "elm_access_eo.c"
