1651ce0c1d09f2c69cbd993d857069c8c638eba2
Hosang Kim <hosang12.kim@samsung.com>
2019-03-06 17:36:22 +0900

    ecore_wl2: revert upstream patch.
    Tizen's enlightenment does not use multiplied value.
    
    @tizen_only
    
    Change-Id: I05788e26aa47eea307b7a5270bf5e35e30f14e90
    (cherry picked from commit fea105fa445ff34039c9452e6c0fa19ae4c3bd42)

--- src/lib/ecore_wl2/ecore_wl2_input.c
+++ src/lib/ecore_wl2/ecore_wl2_input.c
@@ -521,7 +521,9 @@ _ecore_wl2_input_mouse_wheel_send(Ecore_Wl2_Input *input, unsigned int axis, int
    ev->modifiers = input->keyboard.modifiers;
    ev->x = input->pointer.sx;
    ev->y = input->pointer.sy;
-   value /= 10;
+   //TIZEN_ONLY(20190306): revert upstream patch
+   //value /= 10;
+   //
 
    if (axis == WL_POINTER_AXIS_VERTICAL_SCROLL)
      {
