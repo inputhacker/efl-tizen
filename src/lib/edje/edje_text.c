a380c859371eb4266a62e6bd72412208f2557f28
Mike Blumenkrantz <zmike@samsung.com>
2019-03-07 17:42:43 -0500

    evas: restore legacy types which were defined to eo types
    Reviewed-by: Cedric BAIL <cedric.bail@free.fr>
    Differential Revision: https://phab.enlightenment.org/D8245
    
    Change-Id: Icecac2cd81bf67fcd3af69444e984d05438b7841

--- src/lib/edje/edje_text.c
+++ src/lib/edje/edje_text.c
@@ -506,7 +506,7 @@ arrange_text:
       if (params->type.text->align.x < FROM_INT(0))
         {
            /* TIZEN_ONLY(20170216): Apply align according to paragraph_direction
-           if (evas_object_text_direction_get(ep->object) ==
+           if ((Evas_BiDi_Direction)evas_object_text_direction_get(ep->object) ==
                EVAS_BIDI_DIRECTION_RTL)
              {
                 align_x = FROM_INT(1);
