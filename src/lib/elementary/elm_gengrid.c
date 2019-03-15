b4148b3e35e7146fe7246e525edcc28990ae41d8
Marcel Hollerbach <mail@marcel-hollerbach.de>
2019-03-15 14:12:04 +0100

    elm_gengrid: speed up update order calls
    update order can be quite expensive, so this here tries to skip it as
    often as possible.
    
    ref T7384
    
    Reviewed-by: SangHyeon Jade Lee <sh10233.lee@samsung.com>
    Differential Revision: https://phab.enlightenment.org/D8367
    
    Change-Id: I750d4ac6ebbb03f43a6727786316dd313021a87c

--- src/lib/elementary/elm_gengrid.c
+++ src/lib/elementary/elm_gengrid.c
@@ -1370,6 +1370,7 @@ _elm_gengrid_item_unrealize(Elm_Gen_Item *it,
    {
       ELM_GENGRID_DATA_GET_FROM_ITEM(it, sd);
       efl_ui_focus_manager_calc_unregister(sd->obj, EO_OBJ(it));
+      sd->order_dirty = EINA_TRUE;
    }
 
    evas_event_thaw(evas_object_evas_get(WIDGET(it)));
@@ -1727,6 +1728,7 @@ _item_realize(Elm_Gen_Item *it)
    if (!it->realized)
      {
         efl_ui_focus_manager_calc_register_logical(sd->obj, EO_OBJ(it), sd->obj, NULL);
+        sd->order_dirty = EINA_TRUE;
      }
 
    if ((it->realized) ||
@@ -5372,9 +5374,7 @@ _elm_gengrid_item_new(Elm_Gengrid_Data *sd,
    sd->atspi_children = eina_list_append(sd->atspi_children, EO_OBJ(it));
    //
 
-   efl_ui_focus_composition_dirty(sd->obj);
-
-  return it;
+   return it;
 }
 
 EOLIAN static void
@@ -7495,6 +7495,9 @@ _elm_gengrid_efl_ui_focus_object_setup_order(Eo *obj, Elm_Gengrid_Data *pd)
    //TIZEN_ONLY(20180607): Restore legacy focus
    if (elm_widget_is_legacy(obj)) return;
    //
+
+   if (!pd->order_dirty) return;
+   pd->order_dirty = EINA_FALSE;
 
    EINA_INLIST_FOREACH(pd->items, item)
      {
